e12890b06ab504ae0afc98d0facf3fdaf50bdbe9
volodymyr.petrivskyi <vovapetrivskyi@gmail.com>
2024-03-30 10:40:00 +0200

    Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Original object:");
+
+            Record record = new Record();
+            record.Message = "Original message";
+            record.Number = 3;
+
+            record.DisplayInfo();
+
+            Console.WriteLine("Clonning...");
+
+            Thread.Sleep(1000);
+
+            Record copyRecord = record.Clone() as Record;
+
+            copyRecord.DisplayInfo();
+
+            Console.WriteLine("Edit clonned product fields");
+
+            copyRecord.Message = "Coppied message";
+            copyRecord.Number = 7;
+
+            Console.WriteLine("Original object:");
+            record.DisplayInfo();
+
+            Console.WriteLine("Copy of the object");
+            copyRecord.DisplayInfo();
+
+            Console.WriteLine("-----------------------------");
+
+            Record recordNew = record;
+
+            recordNew.Message = "New message";
+            recordNew.Number = 33;
+
+            Console.WriteLine("New object withou clonning but with assign");
+            Console.WriteLine("Original object:");
+            record.DisplayInfo();
+
+            Console.WriteLine("New object");
+            recordNew.DisplayInfo();
+        }
+    }
+}
---
+++ Prototype/Record.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Record : IPrototype
+    {
+        public DateTime CreationTime { get; set; }
+
+        public string Message { get; set; }
+
+        public int Number { get; set; }
+
+        public Record()
+        {
+            CreationTime = DateTime.Now;
+            Message = "";
+        }
+
+        public IPrototype Clone()
+        {
+            return this.MemberwiseClone() as IPrototype;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Message = {this.Message}, Number = {this.Number}, created at {this.CreationTime}");
+        }
+    }
+}
